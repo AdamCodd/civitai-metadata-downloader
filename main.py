9e5b3a6a4c98551330dc43d98553891b0fffd1bb
AdamCodd <49641859+AdamCodd@users.noreply.github.com>
2024-04-19 22:41:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import chromedriver_binary  # Adds chromedriver binary to path
 import json
 import time
 
